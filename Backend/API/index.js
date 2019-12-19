85139f2c54f6ba8bc49939705a326ac18b470383
Hector <hefeligo@gmail.com>
2019-12-19 13:36:59 +0100

    cors cretential:true

--- Backend/API/index.js
+++ Backend/API/index.js
@@ -34,6 +34,7 @@ const server = express();
 //Opciones de CORS
 const corsOptions = {
     origin: 'https://my-static.ga',
+    credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 //========= Middleware ===========
