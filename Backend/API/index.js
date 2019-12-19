acaaeceb8ba111f56339b6a7507ef80d19d88f04
Hector <hefeligo@gmail.com>
2019-12-19 13:32:30 +0100

    cors origin sin /

--- Backend/API/index.js
+++ Backend/API/index.js
@@ -33,7 +33,7 @@ const server = express();
 
 //Opciones de CORS
 const corsOptions = {
-    origin: 'https://my-static.ga/',
+    origin: 'https://my-static.ga',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 //========= Middleware ===========
